c98ec1f458efd72230f6eb9533845837bc3cdce4
Edvard Unsvåg <edvard.unsvag@EdvardUnsvag-sin-MacBook-Pro.local>
2026-01-29 10:13:48 +0100

    fix(comment): add comment to run workflow

--- server/Extensions/AuthenticationExtensions.cs
+++ server/Extensions/AuthenticationExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+//Comment for build fix
 namespace Fortedle.Server.Extensions;
 
 public static class AuthenticationExtensions
